cde4604212a6ea0940dfca79ad8e49c3bed37181
Guilherme Pereira <okkopsiu@gmail.com>
2021-07-26 16:19:22 -0300

    first commit

---
+++ components/links.js
@@ -0,0 +1,9 @@
+import NextLink from 'next/link';
+
+export default function Link({ href, children, ...props }) {
+  return (
+    <NextLink href={href} passHref>
+      <a {...props}>{children}</a>
+    </NextLink>
+  );
+}
